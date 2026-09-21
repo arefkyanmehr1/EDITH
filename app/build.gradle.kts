5f00ccaac4fb9ef2fb253570b74acd826028313d
arefkyanmehr1 <arefkyanmehr9@gmail.com>
2026-09-21 15:03:08 +0330

    Configure E.D.I.T.H. production backend URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,8 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.2.0"
+        buildConfigField("String", "API_BASE_URL", "\"https://arefkyanmehr.ir/instagram/api/\"")
     }
 
     buildFeatures {
