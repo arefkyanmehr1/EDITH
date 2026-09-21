69d423649cc56cddcedec1044ac6a2742b7b28ca
arefkyanmehr1 <arefkyanmehr9@gmail.com>
2026-09-21 19:03:17 +0330

    Add coroutines for EDITH network operations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,5 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20240303")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
 }
